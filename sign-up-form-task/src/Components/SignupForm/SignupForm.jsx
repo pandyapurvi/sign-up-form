348b2596ea6726866394b63fa4ba0ae6831467ae
purvi.pandya <purvi.pandya@service.nsw.gov.au>
2024-06-15 17:18:31 +1000

    fix: added some style to the form

--- sign-up-form-task/src/Components/SignupForm/SignupForm.jsx
+++ sign-up-form-task/src/Components/SignupForm/SignupForm.jsx
@@ -1,71 +1,77 @@
 import React from 'react';
 import styles from './SignupForm.module.css';
 
-export const SignupForm = () => { 
-	const [form, setForm] = React.useState({
-		email: '',
-		name: '',
-		password: ''
-	});
+export const SignupForm = () => {
+  const [form, setForm] = React.useState({
+    email: '',
+    name: '',
+    password: '',
+  });
 
-	const onHandleChange = (e) => {
-		const { name, value } = e.target;
-		setForm({
-			...form,
-			[name]: value
-		});
-	console.log('name', name);
-	}
+  const onHandleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    console.log('name', name);
+  };
 
-	const onHandleSubmitForm = (e) => { 
-		e.preventDefault();
-		console.log('form', form);
-	
-	}
-	return (
-		<div className={styles.fromContainer}>
-			<h1 className={styles.formHeader}>MyWave Sign Up Form</h1>
-			<form onSubmit={onHandleSubmitForm} className={styles.form}> 
-				<div className={styles.formGroup}>
-					<label>Email</label>
-					<input
-						type="text"
-						aria-label="Email field"
-						name="email"
-						placeholder='Email address'
-						value={form.email}
-						onChange={onHandleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label>Password</label>
-					<input
-						type="password"
-						aria-label="Password field"
-						name="password"
-						placeholder='Password'
-						value={form.password}
-						onChange={onHandleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label>Confirm password</label>
-					<input
-						type="password"
-						aria-label="Confirm password field"
-						name="confirmPassword"
-						placeholder='Confirm password'
-						value={form.confirmPassword}
-						onChange={onHandleChange}
-						required
-					/>
-				</div>
-				<button type="submit">Sign Up</button>
-			</form>
-		</div>
-	);
-}
+  const onHandleSubmitForm = (e) => {
+    e.preventDefault();
+    console.log('form', form);
+  };
+  return (
+    <div className={styles.fromContainer}>
+      <h1 className={styles.formHeader}>MyWave Sign Up Form</h1>
+      <form onSubmit={onHandleSubmitForm} className={styles.form}>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Email</label>
+          <input
+            className={styles.formInput}
+            type="text"
+            aria-label="Email field"
+            name="email"
+            placeholder="Your email address"
+            value={form.email}
+            onChange={onHandleChange}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Password</label>
+          <input
+            className={styles.formInput}
+            type="password"
+            aria-label="Password field"
+            name="password"
+            placeholder="Create password"
+            value={form.password}
+            onChange={onHandleChange}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Confirm password</label>
+          <input
+            className={styles.formInput}
+            type="password"
+            aria-label="Confirm password field"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            value={form.confirmPassword}
+            onChange={onHandleChange}
+            required
+          />
+        </div>
+        <div className={styles.btnContainer}>
+          <button type="submit" className={styles.submitBtn}>
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SignupForm;
