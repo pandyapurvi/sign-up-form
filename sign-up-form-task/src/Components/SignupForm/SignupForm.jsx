f7ea5b935db4ad6160bc596f7737cdccc48fc97f
purvi.pandya <purvi.pandya@service.nsw.gov.au>
2024-06-16 17:14:36 +1000

    feat: added few unit tests

--- sign-up-form-task/src/Components/SignupForm/SignupForm.jsx
+++ sign-up-form-task/src/Components/SignupForm/SignupForm.jsx
@@ -5,29 +5,34 @@ import { useFormValidator } from '../FormValidator/useFormValidator';
 export const SignupForm = () => {
   const [form, setForm] = useState({
     email: '',
-		password: '',
-		confirmPassword: ''
-	});
-	
-	const [isValid, setValid] = useState(false);
+    password: '',
+    confirmPassword: '',
+  });
+  const [isValid, setValid] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+	// To handle form input change
   const onHandleChange = (e) => {
     const { name, value } = e.target;
     setForm({
       ...form,
       [name]: value,
     });
+  };
+
+	// calling useFormValidator hook to vlaidate form
+  const { errors, handleBlur, isFormValidate } = useFormValidator(form);
+
+  const onHandleSubmitForm = (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+    if (isFormValidate()) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
 	};
-
-	const { errors, handleBlur, isFormValidate } = useFormValidator(form);
-
-	const onHandleSubmitForm = (e) => {
-		e.preventDefault();
-		if (isFormValidate()) {
-			setValid(true)
-		} else {
-			setValid(false)
-		};
-	}
+	
   return (
     <div className={styles.fromContainer}>
       <h1 className={styles.formHeader}>MyWave Sign Up Form</h1>
@@ -42,9 +47,11 @@ export const SignupForm = () => {
             placeholder="Your email address"
             value={form.email}
             onChange={onHandleChange}
-						onBlur={() => handleBlur('email')}
-					/>
-					{errors.email ? <p className={styles.errorMessage}>{errors.email}</p> : null}
+            onBlur={() => handleBlur('email')}
+          />
+          {errors.email ? (
+            <p className={styles.errorMessage} data-testid='emailError'>{errors.email}</p>
+          ) : null}
         </div>
         <div className={styles.formGroup}>
           <label className={styles.label}>Password</label>
@@ -55,10 +62,12 @@ export const SignupForm = () => {
             name="password"
             placeholder="Create password"
             value={form.password}
-						onChange={onHandleChange}
-						onBlur={() => handleBlur('password')}
-					/>
-					{errors.password ? <p className={styles.errorMessage}>{errors.password}</p> : null}
+            onChange={onHandleChange}
+            onBlur={() => handleBlur('password')}
+          />
+          {errors.password ? (
+            <p className={styles.errorMessage} data-testid='passwordError'>{errors.password}</p>
+          ) : null}
         </div>
         <div className={styles.formGroup}>
           <label className={styles.label}>Confirm password</label>
@@ -71,14 +80,24 @@ export const SignupForm = () => {
             value={form.confirmPassword}
             onChange={onHandleChange}
             onBlur={() => handleBlur('confirmPassword')}
-					/>
-					{errors.confirmPassword ? <p className={styles.errorMessage}>{errors.confirmPassword}</p> : null}
+          />
+          {errors.confirmPassword ? (
+            <p className={styles.errorMessage} data-testid='confirmPasswordError'>{errors.confirmPassword}</p>
+          ) : null}
         </div>
         <div className={styles.btnContainer}>
           <button type="submit" className={styles.submitBtn}>
             Sign Up
-					</button>
-					{isValid ? <p className={styles.submitMessage}>Form is submitted successfully!!</p>: null}
+          </button>
+          {isValid ? (
+            <p className={styles.submitMessage}>
+              Form is submitted successfully!!
+            </p>
+					) : null}
+					{/* When click on form when all fields are empty, show the below message */}
+          {isSubmitted && !isValid ? (
+            <p className={styles.submitMessage}> All fields are required!!</p>
+          ) : null}
         </div>
       </form>
     </div>
--- sign-up-form-task/src/Components/SignupForm/SignupForm.test.js
+++ sign-up-form-task/src/Components/SignupForm/SignupForm.test.js
@@ -1,8 +1,8 @@
 import SignupForm from "./SignupForm";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-describe("SignupForm", () => { 
-	it("should render the form", () => { 
+describe("SignupForm", () => {
+	it("should render the form", () => {
 		render(<SignupForm />);
 		expect(screen.getByText('MyWave Sign Up Form')).toBeInTheDocument();
 		expect(screen.getByLabelText('Email field')).toBeInTheDocument();
@@ -24,5 +24,53 @@ describe("SignupForm", () => {
 		fireEvent.change(confirmPasswordInput, {
 			target: { value: 'Password@1234' }
 		});
+	});
+	it("should show error message when user left email filed after click", () => {
+		render(<SignupForm />);
+		const emailInput = screen.getByLabelText('Email field');
+		const passwordInput = screen.getByLabelText('Password field');
+		const confirmPasswordInput = screen.getByLabelText('Confirm password field');
+		fireEvent.blur(emailInput);
+		fireEvent.blur(passwordInput);
+		fireEvent.blur(confirmPasswordInput);
+		expect(screen.getByTestId('emailError')).toHaveTextContent('Email is required');
+		expect(screen.getByTestId('passwordError')).toHaveTextContent('Password is required');
+		expect(screen.getByTestId('confirmPasswordError')).toHaveTextContent('Confirm password is required');
+	});
+	it("should show error message when user add invalid email", () => {
+		render(<SignupForm />);
+		const emailInput = screen.getByLabelText('Email field');
+		fireEvent.change(emailInput, {
+			target: { value: 'test' }
+		});
+		fireEvent.blur(emailInput);
+		expect(screen.getByTestId('emailError')).toHaveTextContent('Email is invalid');
+	});
+	it("should show error message when user add invalid password", () => {
+		render(<SignupForm />);
+		const passwordInput = screen.getByLabelText('Password field');
+		fireEvent.change(passwordInput, {
+			target: { value: 'test123' }
+		});
+		fireEvent.blur(passwordInput);
+		expect(screen.getByTestId('passwordError')).toHaveTextContent('Password must be 8 characters');
+	});
+	it("should show text when sunmit button is clicked", () => {
+		render(<SignupForm />);
+		const emailInput = screen.getByLabelText('Email field');
+		const passwordInput = screen.getByLabelText('Password field');
+		const confirmPasswordInput = screen.getByLabelText('Confirm password field');
+		const submitBtn = screen.getByRole('button');
+		fireEvent.change(emailInput, {
+			target: { value: 'test123@test.com' }
+		});
+		fireEvent.change(passwordInput, {
+			target: { value: 'Password@1234' }
+		});
+		fireEvent.change(confirmPasswordInput, {
+			target: { value: 'Password@1234' }
+		});
+		fireEvent.click(submitBtn);
+		expect(screen.getByText('Form is submitted successfully!!')).toBeInTheDocument();
 	})
-})
+});
